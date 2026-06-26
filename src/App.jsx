2fc87fb866062a8d1cf070caa77713c03b0cb5ab
aba-geomdan <abageomdan@gmail.com>
2026-06-26 22:32:36 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-aimport React, { useState, useEffect, useMemo, useRef } from 'react';
+import React, { useState, useEffect, useMemo, useRef } from 'react';
 import * as XLSX from 'xlsx';
 import {
   authLogin,
