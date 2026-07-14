480a312188ef03b9414f236f0f4dca78fa920817
aba-geomdan <abageomdan@gmail.com>
2026-07-14 10:34:40 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,29 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import obfuscator from 'vite-plugin-javascript-obfuscator';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    obfuscator({
+      apply: 'build',
+      include: [/\.js$/],
+      exclude: [/node_modules/],
+      debugger: false,
+      options: {
+        compact: true,
+        stringArray: true,
+        stringArrayEncoding: ['base64'],
+        stringArrayThreshold: 0.75,
+        identifierNamesGenerator: 'hexadecimal',
+        controlFlowFlattening: false,
+        deadCodeInjection: false,
+        debugProtection: false,
+        selfDefending: false
+      }
+    })
+  ],
   base: '/geomdan-tracker/', // GitHub Pages 저장소 이름과 일치해야 함
   build: {
     outDir: 'dist',
