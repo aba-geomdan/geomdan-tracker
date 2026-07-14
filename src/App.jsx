d1dac2eb0d3e4e574eff90711de1b6e8bf9259f8
aba-geomdan <abageomdan@gmail.com>
2026-07-14 14:39:49 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4153,17 +4153,17 @@ function LoginScreen({ onLogin }) {
         <input type="text" name="username" tabIndex={-1} aria-hidden="true" autoComplete="username" style={{ position: 'absolute', opacity: 0, height: 0, width: 0, pointerEvents: 'none', zIndex: -1 }} />
         <input type="password" name="password" tabIndex={-1} aria-hidden="true" autoComplete="current-password" style={{ position: 'absolute', opacity: 0, height: 0, width: 0, pointerEvents: 'none', zIndex: -1 }} />
 
-        {/* 아이디 */}
+        {/* 이메일 */}
         <div style={{ marginBottom: 12 }}>
           <label style={{ display: 'block', fontSize: 11, fontWeight: 600, color: C.grayHead, marginBottom: 5 }}>
-            아이디
+            이메일
           </label>
           <input
             type="text"
             value={userId}
             onChange={async (e) => { setUserId(e.target.value); setError(''); }}
             onKeyDown={handleKeyDown}
-            placeholder="아이디 입력"
+            placeholder="이메일 입력"
             autoFocus
             autoComplete="off"
             autoCorrect="off"
